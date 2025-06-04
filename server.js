744d838629482a4c745a549ab687ff26003d8d21
honepoli <43112272+honepoli@users.noreply.github.com>
2025-06-04 20:10:30 +0900

    Hide signup when signed in

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'signup.html'));
+});
+
+app.get('/signin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'signin.html'));
+});
+
+app.get('/user', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'user.html'));
+});
 
 // Simple in-memory session store
 const sessions = new Map();
